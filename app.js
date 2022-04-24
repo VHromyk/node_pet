5db52287396aa38aa928784d236af6657952de24
Vitalii Hromyk <hromykk@gmail.com>
2022-04-24 23:58:06 +0300

    added form

--- app.js
+++ app.js
@@ -6,11 +6,22 @@ const { people } = require('./express-tutorial/data');
 // static assets 
 app.use(express.static('./express-tutorial/methods-public'))
 // parse form data
-
 app.use(express.urlencoded({ extended: false }))
+//parse json
+app.use(express.json())
 
 app.get('/api/people', (req, res) => {
     res.status(200).json({ success: true, data: people })
+})
+
+app.post('/api/people', (req, res) => {
+    const { name } = req.body;
+    if (name) {
+        return res.status(201).json({success: true, person: name})
+    }
+    return res
+        .status(400)
+        .json({ success: false, msg: 'please provide credentionals' });
 })
 
 app.post('/login', (req, res) => {
@@ -18,11 +29,9 @@ app.post('/login', (req, res) => {
     if (name) {
         return res.status(200).send(`Welcome ${name}`)
     }
-    res.status(401).send('Please Provide Credentionals');
+    return res.status(400).json({success: false, msg: "please provide credentionals"});
     
 })
-
-
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000...')
